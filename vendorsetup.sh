5dbed704d22a4b7a71e15fd5facef0baaa18d821
electimon <electimon@gmail.com>
2022-05-14 11:23:45 +0000

    amogus: Initial Commit!
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_amogus-user
+add_lunch_combo twrp_amogus-userdebug
+add_lunch_combo twrp_amogus-eng
+export OF_AB_DEVICE=1
+export TARGET_DEVICE_ALT="sofiar sofia rav rav_t sofiap sofiap_sprout"
